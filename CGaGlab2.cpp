62999a7b451f313239ab468ca9e102f646a5f5cf
BlackFairyL <45395109+BlackFairyL@users.noreply.github.com>
2020-05-02 16:07:17 +0300

    Вторая лабораторная работа

---
+++ CGaGlab2.cpp
@@ -0,0 +1,151 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>	
+#include <vector>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+const double EPS = 1e-5;
+
+void to_black(vector<unsigned char> &pixel) {
+	pixel.assign(pixel.size(), 0);
+}
+
+void draw_point(vector<unsigned char> &pixel, int &x, int &y, int &mx, int &my, int bright, double &gamma, char &type) {
+	if (bright == 0) {
+		return;
+	}
+	if (type == '5') {
+		pixel[x * my + y] = pow(bright / 255.0, 1. / gamma) * 255;
+	}
+	else {
+		pixel[x * my * 3 + y * 3] = pow(bright / 255.0, 1. / gamma) * 255;
+		pixel[x * my * 3 + y * 3 + 1] = pow(bright / 255.0, 1. / gamma) * 255;
+		pixel[x * my * 3 + y * 3 + 2] = pow(bright / 255.0, 1. / gamma) * 255;
+	}
+}
+
+double dist(double &x1, double &y1, double &x2, double &y2) {
+	double dx = x1 - x2, dy = y1 - y2;
+	return sqrt(dx * dx + dy * dy);
+}
+
+double min_dist(double &xs, double &ys, double &xf, double &yf, double findx, double findy) {
+	double to_st = dist(xs, ys, findx, findy), to_fi = dist(xf, yf, findx, findy), len = dist(xs, ys, xf, yf), temp = 0.0;
+	double A = yf - ys, B = xf - xs, C = ys * xf - yf * xs, H;
+	H = fabs(A * findx - B * findy + C) / sqrt(A * A + B * B);
+	if (to_st >= dist(to_fi, len, temp, temp) || to_fi >= dist(to_st, len, temp, temp))
+		H = to_st;
+	return min(min(to_st, to_fi), H);
+}
+
+double normal(double dist, double &thick) {
+	if (dist >= thick + 1.0) // расстояние до точки больше толщины линии и погрешности -> не красим
+		return 0.0;
+	if (dist <= thick) // точка есть часть прямой, яркость как есть
+		return 1.0;
+	return 1.0 - dist + thick; // высчитываем погрешность, чем выше погрешность - тем тусклее точка
+}
+
+void draw_line(vector<unsigned char> &pixel, double &xs, double &ys, double &xf, double&yf, int &mx, int &my, int &bright, double &thick, double &gamma, char &type) {
+	if (xs > xf) {
+		swap(xs, xf);
+		swap(ys, yf);
+	}
+	double thick_cor = thick + 2.0 - EPS;
+	// direct line
+	if (xs == xf || ys == yf)
+		for (int x = max(0, int(xs - thick_cor)); x <= min(mx - 1, int(xf + thick_cor)); x++)
+			for (int y = max(0, int(ys - thick_cor)); y <= min(my - 1, int(yf + thick_cor)); y++)
+				draw_point(pixel, x, y, mx, my, bright * normal(min_dist(xs, ys, xf, yf, x, y), thick), gamma, type);
+	else {
+		//алгоритм Ву
+		double grad = (yf - ys) / (xf - xs), ynow = ys;
+		for (int x = max(0, int(xs - thick_cor)); x <= min(mx, int(xf + thick_cor)); x++) {
+			for (int y = max(0, int(ynow - fabs(grad) - thick_cor)); y <= min(my, int(ynow + fabs(grad) + thick_cor)); y++)
+				draw_point(pixel, x, y, mx, my, bright * normal(min_dist(xs, ys, xf, yf, x, y), thick), gamma, type);
+			ynow += (x >= xs && x <= xf ? grad : 0);
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+
+	if (argc != 10 && argc != 9) {
+		cerr << "Invalid request";
+		return 1;
+	}
+	string name_in = argv[1];
+	string name_out = argv[2];
+	int bright = atoi(argv[3]);
+	double	thick = (atof(argv[4]) - 1) / 2.0,	// корректируем толщину
+			xs = atof(argv[5]),
+			ys = atof(argv[6]),
+			xf = atof(argv[7]),
+			yf = atof(argv[8]),
+		gamma = (argc == 10 ? atof(argv[9]) : 2.2);
+	//kgig.exe <имя_входного_файла> <имя_выходного_файла> <яркость_линии> <толщина_линии> <x_начальный> <y_начальный> <x_конечный> <y_конечный> <гамма>
+
+	FILE * fin = fopen(name_in.c_str(), "rb");
+	if (!fin) {
+		cerr << "Invalid input file";
+		return 1;
+	}
+
+	char p, number;
+	int h, w, color;
+	int count = fscanf(fin, "%c%c\n%d %d\n%d\n", &p, &number, &w, &h, &color);
+	if (count != 5) {
+		cerr << "Incorrect file content";
+		return 1;
+	}
+
+	if (!(p == 'P' && (number == '5' || number == '6')) || h <= 0 || w <= 0 || color > 255 || color <= 0) {
+		cerr << "Incorrect data in the file";
+		return 1;
+	}
+	bool flag = 0;
+	vector<unsigned char> pixel;
+	if (number == '5') {
+		pixel.resize(h * w, 0);
+	}
+	else if (number == '6') {
+		pixel.resize(3 * h * w);
+		flag = 1;
+	}
+
+	int cnt = fread(&pixel[0], sizeof(unsigned char), pixel.size(), fin);
+	if (number == '5' && cnt != h * w || number == '6' && cnt != 3 * h * w) {
+		cerr << "Incorrect data in the file";
+		return 1;
+	}
+
+	if (!(0 <= xs && xs < h &&
+		0 <= xf && xf < h &&
+		0 <= ys && ys < w &&
+		0 <= yf && yf < w &&
+		0 <= bright && bright <= 255 &&
+		-0.5 < thick &&
+		0 < gamma)) {
+		cerr << "Incorrect arguments in console";
+		return 1;
+	}
+
+	fclose(fin);
+	//to_black(pixel);
+	draw_line(pixel, xs, ys, xf, yf, h, w, bright, thick, gamma, number);
+
+	FILE* fout = fopen(name_out.c_str(), "wb");
+
+	if (!fout) {
+		cerr << "Invalid output file";
+		return 1;
+	}
+
+	fprintf(fout, "%c%c\n%d %d\n%d\n", p, number, w, h, color);
+	fwrite(&pixel[0], sizeof(unsigned char), pixel.size(), fout);
+	fclose(fout);
+	return 0;
+}
